23256a4ca5949a9ecf309b3c768be91d44918689
Yifei <yifeizhou.to@gmail.com>
2019-05-30 18:06:47 +0000

    completed loaded die

---
+++ loaded-die/loaded-die.js
@@ -0,0 +1,19 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+  var index = 0;
+
+  return function() {
+    var nextRoll = list[index];
+    index++;
+
+    return nextRoll;
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
